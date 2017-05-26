754b9a9b18d9ef66059043190eeb8ccbe853dba9
Brett Bennett <bbennett905@gmail.com>
2017-05-26 18:18:21 -0400

    Fix window size

--- csch8/MainForm.Designer.cs
+++ csch8/MainForm.Designer.cs
@@ -60,7 +60,7 @@
             this.debugToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(640, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -237,7 +237,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 305);
+            this.ClientSize = new System.Drawing.Size(640, 344);
             this.Controls.Add(this.fpsSelector);
             this.Controls.Add(this.pauseButton);
             this.Controls.Add(this.opcode_label);
